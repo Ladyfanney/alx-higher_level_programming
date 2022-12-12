8b7b886d4e895a157768bc8baf7c47c2a4865e88
fanney <fanneyokato@gmail.com>
2022-12-12 02:58:38 -0800

    How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if size > 1:
         print("{} arguments:".format(size))
         for i in range(1, size + 1):
-             print("{}: {}".format(i, arg[i]))
+            print("{}: {}".format(i, arg[i]))
 
     elif size == 0:
         print("{} arguments.".format(size))
